fd70555875ca66cbb99b732568f3fcdcb425bffc
nishantshah45 <nishantshah021351@gmail.com>
2025-06-08 17:50:38 +0545

    image
    lab sheet

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.load = new System.Windows.Forms.Button();
+            this.lowpass = new System.Windows.Forms.Button();
+            this.gaussian = new System.Windows.Forms.Button();
+            this.laplace = new System.Windows.Forms.Button();
+            this.highpass = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(64, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 396);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(688, 53);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 396);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(454, 87);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(75, 23);
+            this.load.TabIndex = 2;
+            this.load.Text = "load";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // lowpass
+            // 
+            this.lowpass.Location = new System.Drawing.Point(454, 233);
+            this.lowpass.Name = "lowpass";
+            this.lowpass.Size = new System.Drawing.Size(75, 23);
+            this.lowpass.TabIndex = 3;
+            this.lowpass.Text = "MeanLowPass";
+            this.lowpass.UseVisualStyleBackColor = true;
+            this.lowpass.Click += new System.EventHandler(this.lowpass_Click);
+            // 
+            // gaussian
+            // 
+            this.gaussian.Location = new System.Drawing.Point(454, 297);
+            this.gaussian.Name = "gaussian";
+            this.gaussian.Size = new System.Drawing.Size(75, 23);
+            this.gaussian.TabIndex = 4;
+            this.gaussian.Text = "Gaussianfilte";
+            this.gaussian.UseVisualStyleBackColor = true;
+            // 
+            // laplace
+            // 
+            this.laplace.Location = new System.Drawing.Point(454, 166);
+            this.laplace.Name = "laplace";
+            this.laplace.Size = new System.Drawing.Size(75, 23);
+            this.laplace.TabIndex = 5;
+            this.laplace.Text = "laplacianfilter";
+            this.laplace.UseVisualStyleBackColor = true;
+            this.laplace.Click += new System.EventHandler(this.laplace_Click);
+            // 
+            // highpass
+            // 
+            this.highpass.Location = new System.Drawing.Point(454, 388);
+            this.highpass.Name = "highpass";
+            this.highpass.Size = new System.Drawing.Size(75, 23);
+            this.highpass.TabIndex = 6;
+            this.highpass.Text = "highpass";
+            this.highpass.UseVisualStyleBackColor = true;
+            this.highpass.Click += new System.EventHandler(this.highpass_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(653, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Lab 3 Spatial Filtering: Applying filters such as averaging (Mean or Lowpas) ,Hih" +
+                "hpass and Gaussian to smooth images and redue noise of";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 529);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.highpass);
+            this.Controls.Add(this.laplace);
+            this.Controls.Add(this.gaussian);
+            this.Controls.Add(this.lowpass);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.Button lowpass;
+        private System.Windows.Forms.Button gaussian;
+        private System.Windows.Forms.Button laplace;
+        private System.Windows.Forms.Button highpass;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Bitmap originalBitmap;
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofg = new OpenFileDialog();
+            if (ofg.ShowDialog() == DialogResult.OK)
+            {
+                originalBitmap = new Bitmap(ofg.FileName);
+
+                pictureBox1.Image = originalBitmap;
+                originalBitmap = ToGrayScale((Bitmap)pictureBox1.Image);
+                ShowImage(originalBitmap, "Original IMage(Gray Scale)");
+
+
+            }
+
+        }
+
+        static Bitmap ToGrayScale(Bitmap Original)
+        {
+            Bitmap Gray = new Bitmap(Original.Width, Original.Height);
+            for (int x = 0; x < Original.Width; x++)
+            {
+                for (int y = 0; y < Original.Height; y++)
+                {
+                    Color pixel = Original.GetPixel(x, y);
+                    int grayVal = (int)(0.299 * pixel.R + 0.587 * pixel.G + 0.114 * pixel.B);
+                    Gray.SetPixel(x, y, Color.FromArgb(grayVal, grayVal, grayVal));
+                }
+            }
+
+            return Gray;
+        }
+
+        static void ShowImage(Bitmap bmp, string title)
+        {
+            Form form = new Form();
+            form.Text = title;
+            form.ClientSize = new Size(bmp.Width, bmp.Height);
+            PictureBox pb = new PictureBox();
+            pb.Dock = DockStyle.Fill;
+            pb.Image = bmp;
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            form.Controls.Add(pb);
+            form.Show();
+
+        }
+        private Bitmap MeanFilter(Bitmap bmp)
+        {
+
+            Bitmap result = new Bitmap(bmp.Width, bmp.Height);
+            for (int y = 1; y < bmp.Height - 1; y++)
+            {
+                for (int x = 1; x < bmp.Width; x++)
+                {
+                    int sumR = 0, sumG = 0, sumB = 0;
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        for (int i = -1; i <= -1; i++)
+                        {
+                            Color pixel = bmp.GetPixel(x + i, y + j);
+                            sumR += pixel.R;
+                            sumB += pixel.B;
+                            sumG += pixel.G;
+
+                        }
+
+                    }
+
+                    int avR = sumR / 9;
+                    int avG = sumG / 9;
+                    int avB = sumB / 9;
+                    result.SetPixel(x, y, Color.FromArgb(avR, avG, avB));
+                }
+            }
+
+            return result;
+        }
+
+        private void lowpass_Click(object sender, EventArgs e)
+        {
+            if (originalBitmap != null)
+            {
+                pictureBox2.Image = MeanFilter(originalBitmap);
+                ShowImage((Bitmap)pictureBox2.Image, "Using Mean Filter");
+
+            }
+        }
+        static Bitmap Applylaplacian(Bitmap gray)
+        {
+            int[,] kernel = new int[,]
+        {
+        {0,-1,0},
+        {-1,4,-1},
+        {0,-1,0}
+        };
+
+            int width = gray.Width;
+            int Heght = gray.Height;
+            Bitmap result = new Bitmap(width, Heght);
+
+            for (int x = 1; x < width - 1; x++)
+            {
+                for (int y = 1; y < Heght - 1; y++)
+                {
+                    int sum = 0;
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int pixelval = gray.GetPixel(x + i, y + j).R;
+                            sum += pixelval * kernel[i + 1, j + 1];
+
+                        }
+                    }
+
+                    sum = Math.Min(255, Math.Max(0, sum));
+
+                    Color edgeColor = Color.FromArgb(sum, sum, sum);
+                    result.SetPixel(x, y, edgeColor);
+                }
+
+            }
+            return result;
+        }
+
+
+        private Bitmap Highpass(Bitmap bmp)
+        {
+            Bitmap result = new Bitmap(bmp.Width, bmp.Height);
+            int[,] kernel ={
+                          {-1,-1,-1},
+                          {-1, 8,-1},
+                          {-1,-1,-1}
+                          };
+            for (int y = 1; y < bmp.Height - 1; y++)
+            {
+                for (int x = 1; x < bmp.Width - 1; x++)
+                {
+                    int sumR = 0, sumG = 0, sumB = 0;
+
+
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            Color pixel = bmp.GetPixel(x + i, y + j);
+                            int k = kernel[j + 1, i + 1];
+
+                            sumR += pixel.R * k;
+                            sumG += pixel.G * k;
+                            sumB += pixel.B * k;
+                        }
+                    }
+                    sumR = Math.Min(Math.Max(sumR, 0), 255);
+                    sumB = Math.Min(Math.Max(sumB, 0), 255);
+                    sumG = Math.Min(Math.Max(sumG, 0), 255);
+
+                    result.SetPixel(x, y, Color.FromArgb(sumR, sumG, sumB));
+                }
+
+            }
+            return result;
+
+        }
+
+        private void laplace_Click(object sender, EventArgs e)
+        {
+            if (originalBitmap != null)
+            {
+                pictureBox2.Image = Applylaplacian(originalBitmap);
+                ShowImage((Bitmap)pictureBox2.Image, "Using Laplacian Filter");
+            }
+        }
+
+
+
+
+        private void highpass_Click(object sender, EventArgs e)
+        {
+            if (originalBitmap != null)
+            {
+                pictureBox1.Image = Highpass(originalBitmap);
+                ShowImage((Bitmap)pictureBox1.Image, "Using high Filter");
+            }
+        }
+    }
+}
